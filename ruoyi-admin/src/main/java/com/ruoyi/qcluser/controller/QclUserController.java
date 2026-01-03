cf42c4d07eb6721e7493e42ec0afed6173d155a6
wangchangzhen <3047969285@qq.com>
2026-01-03 21:43:31 +0800

    修改

---
+++ ruoyi-admin/src/main/java/com/ruoyi/qcluser/controller/QclUserController.java
@@ -0,0 +1,104 @@
+package com.ruoyi.qcluser.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.qcluser.domain.QclUser;
+import com.ruoyi.qcluser.service.IQclUserService;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.page.TableDataInfo;
+
+/**
+ * 用户Controller
+ * 
+ * @author ruoyi
+ * @date 2025-11-03
+ */
+@RestController
+@RequestMapping("/qcluser/qcluser")
+public class QclUserController extends BaseController
+{
+    @Autowired
+    private IQclUserService qclUserService;
+
+    /**
+     * 查询用户列表
+     */
+    @PreAuthorize("@ss.hasPermi('qcluser:qcluser:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(QclUser qclUser)
+    {
+        startPage();
+        List<QclUser> list = qclUserService.selectQclUserList(qclUser);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出用户列表
+     */
+    @PreAuthorize("@ss.hasPermi('qcluser:qcluser:export')")
+    @Log(title = "用户", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, QclUser qclUser)
+    {
+        List<QclUser> list = qclUserService.selectQclUserList(qclUser);
+        ExcelUtil<QclUser> util = new ExcelUtil<QclUser>(QclUser.class);
+        util.exportExcel(response, list, "用户数据");
+    }
+
+    /**
+     * 获取用户详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('qcluser:qcluser:query')")
+    @GetMapping(value = "/{id}")
+    public AjaxResult getInfo(@PathVariable("id") Long id)
+    {
+        return success(qclUserService.selectQclUserById(id));
+    }
+
+    /**
+     * 新增用户
+     */
+    @PreAuthorize("@ss.hasPermi('qcluser:qcluser:add')")
+    @Log(title = "用户", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody QclUser qclUser)
+    {
+        return toAjax(qclUserService.insertQclUser(qclUser));
+    }
+
+    /**
+     * 修改用户
+     */
+    @PreAuthorize("@ss.hasPermi('qcluser:qcluser:edit')")
+    @Log(title = "用户", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody QclUser qclUser)
+    {
+        return toAjax(qclUserService.updateQclUser(qclUser));
+    }
+
+    /**
+     * 删除用户
+     */
+    @PreAuthorize("@ss.hasPermi('qcluser:qcluser:remove')")
+    @Log(title = "用户", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{ids}")
+    public AjaxResult remove(@PathVariable Long[] ids)
+    {
+        return toAjax(qclUserService.deleteQclUserByIds(ids));
+    }
+}
---
+++ ruoyi-admin/src/main/java/com/ruoyi/qcluser/mapper/QclUserMapper.java
@@ -0,0 +1,61 @@
+package com.ruoyi.qcluser.mapper;
+
+import java.util.List;
+import com.ruoyi.qcluser.domain.QclUser;
+
+/**
+ * 用户Mapper接口
+ * 
+ * @author ruoyi
+ * @date 2025-11-03
+ */
+public interface QclUserMapper 
+{
+    /**
+     * 查询用户
+     * 
+     * @param id 用户主键
+     * @return 用户
+     */
+    public QclUser selectQclUserById(Long id);
+
+    /**
+     * 查询用户列表
+     * 
+     * @param qclUser 用户
+     * @return 用户集合
+     */
+    public List<QclUser> selectQclUserList(QclUser qclUser);
+
+    /**
+     * 新增用户
+     * 
+     * @param qclUser 用户
+     * @return 结果
+     */
+    public int insertQclUser(QclUser qclUser);
+
+    /**
+     * 修改用户
+     * 
+     * @param qclUser 用户
+     * @return 结果
+     */
+    public int updateQclUser(QclUser qclUser);
+
+    /**
+     * 删除用户
+     * 
+     * @param id 用户主键
+     * @return 结果
+     */
+    public int deleteQclUserById(Long id);
+
+    /**
+     * 批量删除用户
+     * 
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteQclUserByIds(Long[] ids);
+}
