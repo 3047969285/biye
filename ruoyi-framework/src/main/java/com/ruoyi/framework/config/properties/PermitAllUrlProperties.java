f324ab0e2e8c1274519466c22e82489b2e268829
wangchangzhen <3047969285@qq.com>
2026-04-15 10:56:06 +0800

    Revert "fix[电表]：增加电表，修改bug"
    This reverts commit 10f41585474e8c7ca67619e990c4da6eacaee3b7.

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/properties/PermitAllUrlProperties.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/properties/PermitAllUrlProperties.java
@@ -1,9 +1,9 @@
 package com.ruoyi.framework.config.properties;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.RegExUtils;
@@ -14,8 +14,6 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.mvc.condition.PathPatternsRequestCondition;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import com.ruoyi.common.annotation.Anonymous;
@@ -47,12 +45,12 @@ public class PermitAllUrlProperties implements InitializingBean, ApplicationCont
 
             // 获取方法上边的注解 替代path variable 为 *
             Anonymous method = AnnotationUtils.findAnnotation(handlerMethod.getMethod(), Anonymous.class);
-            Optional.ofNullable(method).ifPresent(anonymous -> resolvePatterns(info)
+            Optional.ofNullable(method).ifPresent(anonymous -> Objects.requireNonNull(info.getPatternsCondition().getPatterns())
                     .forEach(url -> urls.add(RegExUtils.replaceAll(url, PATTERN, ASTERISK))));
 
             // 获取类上边的注解, 替代path variable 为 *
             Anonymous controller = AnnotationUtils.findAnnotation(handlerMethod.getBeanType(), Anonymous.class);
-            Optional.ofNullable(controller).ifPresent(anonymous -> resolvePatterns(info)
+            Optional.ofNullable(controller).ifPresent(anonymous -> Objects.requireNonNull(info.getPatternsCondition().getPatterns())
                     .forEach(url -> urls.add(RegExUtils.replaceAll(url, PATTERN, ASTERISK))));
         });
     }
@@ -71,24 +69,5 @@ public class PermitAllUrlProperties implements InitializingBean, ApplicationCont
     public void setUrls(List<String> urls)
     {
         this.urls = urls;
-    }
-
-    /**
-     * Spring MVC 6 可能使用 PathPatternsRequestCondition，getPatternsCondition() 可能为 null。
-     */
-    private Collection<String> resolvePatterns(RequestMappingInfo info)
-    {
-        List<String> patterns = new ArrayList<>();
-        PatternsRequestCondition patternsCondition = info.getPatternsCondition();
-        if (patternsCondition != null)
-        {
-            patterns.addAll(patternsCondition.getPatterns());
-        }
-        PathPatternsRequestCondition pathPatternsCondition = info.getPathPatternsCondition();
-        if (pathPatternsCondition != null)
-        {
-            pathPatternsCondition.getPatterns().forEach(p -> patterns.add(p.getPatternString()));
-        }
-        return patterns;
     }
 }
